7c406250975a07001dff24395932d80c0aeac348
Johnny Yeng <a2902793@gmail.com>
2020-12-03 20:58:10 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+pip3 install opencv-python
+sudo apt-get update
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libjasper-dev 
+sudo apt-get install -y libqtgui4 
+sudo apt-get install -y libqt4-test
